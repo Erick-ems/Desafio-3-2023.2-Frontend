fc2c1398d6a33171a4c1e7458bd667d97a3316b2
erick-ems <erick.ems@outlook.com>
2023-11-24 22:48:23 -0300

    terceiro projeto iniciado

---
+++ Des_Front/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import KanbanBoard from './components/KanbanBoard'
+import Task from './components/Task'
+
+function App() {
+
+
+  return (
+    <>
+      <Task task= {{id:123, title: "Make a progress board aplication"}}/>
+      <KanbanBoard/>
+    </>
+  )
+}
+
+export default App
---
+++ Des_Front/src/components/KanbanBoard.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
+import Column from './Column';
+import { useEffect, useState } from "react";
+
+
+export default function KanbanBoard(){
+
+  const [complete, setCompleted] = useState([]);
+  const [incomplete, setIncomplete] = useState([]);
+
+  return (
+
+    <DragDropContext>
+      <h2 style={{ textAlign: 'center' }}>PROGRESS BOARD</h2>
+
+      <div style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+      }}>
+        <Column title={"TO DO"} tasks={incomplete} id={"1"}/>
+
+      </div>
+    </DragDropContext>
+
+
+  );
+}
