0f184010514505a6253878dc1047a8d96ed5cefa
Erick Santos <erick.ems@outlook.com>
2023-11-30 19:16:29 -0300

    projeto nao finalizado

--- Des_Front/src/App.jsx
+++ Des_Front/src/App.jsx
@@ -1,16 +1,14 @@
-import './App.css'
-import KanbanBoard from './components/KanbanBoard'
-import Task from './components/Task'
+import "./App.css";
+import KanbanBoard from "./components/KanbanBoard";
+import Task from "./components/Task";
 
 function App() {
-
-
   return (
     <>
-      <Task task= {{id:123, title: "Make a progress board aplication"}}/>
-      <KanbanBoard/>
+      {/* <Task task= {{id:123, title: "Make a progress board aplication"}}/> */}
+      <KanbanBoard />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
